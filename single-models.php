2b2b97a0205d0dab5001438e6aed0df0a58a9d43
Oleg Pushkash <68034157+WorentWalker@users.noreply.github.com>
2021-12-17 17:54:35 +0200

    video final fix (maybe)

--- single-models.php
+++ single-models.php
@@ -26,6 +26,7 @@ $id = get_the_ID();
     $hair = get_post_meta($id, 'hair', true );
     $eyes = get_post_meta($id, 'eyes', true );
     $age = get_post_meta($id, 'age', true );
+    $about = get_post_meta($id, 'about', true );
 
     $post = get_post( $id );
 
@@ -62,7 +63,7 @@ $id = get_the_ID();
 <div class="model-wr">
 
     <section class="model">
-        <div class="madel-info-back"><a href="/catalog">Back to models</a></div>
+       
         <div class="madel-info-tit mobile">
             <h1><?= stristr($post->post_title, '-', true)?></h1>  
         </div>
@@ -83,74 +84,94 @@ $id = get_the_ID();
                 <?php endforeach; ?>
 
             </ul>
-        <div class="madel-slide">
-            <?php
-            if ( $image_array ) {
 
-                foreach ( $image_array as $image ) {
-
-                    $fullimg = pods_image_url( $image['ID'], 'slideBig');
-
-                    ?>
-                    <div><img src="<?=$fullimg?>" alt=""></div>
-            <?php
-                }
-            }
-            ?>
-        </div>
-        <div class="madel-info">
-
+            <div class="madel-info">
+            <div class="madel-info-back"><a href="/catalog">Back to models</a></div>
             <div class="madel-info-tit">
                 <h1 data-desc="Proffile"><?= stristr($post->post_title, '-', true)?>
                 </h1>
+                <a id="js-selected" data-id="<?=$id?>" href="#" class='add_to_list_heart'>
+                <svg width="46" height="41" viewBox="0 0 46 41" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M44.8871 8.40214C44.1734 6.75794 43.1442 5.26798 41.8573 4.01567C40.5695 2.75962 39.051 1.76146 37.3846 1.07546C35.6566 0.361301 33.8033 -0.00424759 31.9321 3.72366e-05C29.3071 3.72366e-05 26.7459 0.715222 24.5202 2.06613C23.9877 2.38928 23.4819 2.74423 23.0026 3.13096C22.5234 2.74423 22.0176 2.38928 21.4851 2.06613C19.2594 0.715222 16.6982 3.72366e-05 14.0731 3.72366e-05C12.1829 3.72366e-05 10.3512 0.360279 8.62066 1.07546C6.94871 1.76416 5.44183 2.75482 4.14793 4.01567C2.85934 5.26656 1.82998 6.75688 1.11818 8.40214C0.378053 10.1133 0 11.9304 0 13.8005C0 15.5646 0.362079 17.4029 1.08091 19.2729C1.6826 20.8358 2.5452 22.4568 3.64741 24.0938C5.3939 26.6844 7.79534 29.3862 10.7772 32.1251C15.7185 36.6652 20.6119 39.8014 20.8195 39.9285L22.0815 40.7338C22.6406 41.0887 23.3594 41.0887 23.9185 40.7338L25.1804 39.9285C25.3881 39.7961 30.2762 36.6652 35.2228 32.1251C38.2046 29.3862 40.606 26.6844 42.3525 24.0938C43.4547 22.4568 44.3227 20.8358 44.919 19.2729C45.6379 17.4029 45.9999 15.5646 45.9999 13.8005C46.0053 11.9304 45.6272 10.1133 44.8871 8.40214Z" fill="#F02424"/>
+                </svg>
+
+
+                </a>
             </div>
-            <ul class="madel-info-cat">
+<ul class="madel-info-cat">
 
-                <?php foreach ($categories as $category) : ?>
+    <?php foreach ($categories as $category) : ?>
 
-                <?php
-                    if($category->term_id == 1 || $category->slug == 'nude' || $category->slug == 'topless') continue;
+    <?php
+        if($category->term_id == 1 || $category->slug == 'nude' || $category->slug == 'topless') continue;
 
-                    $isCat = false;
-                    if (in_array($category->term_id, $categoriesModel)) {
-                        $isCat = true;
-                    }
-                    ?>
-                    <?php $class = $isCat ? 'class="active-cat"': '' ?>
-                    <li <?=$class?>><?=$category->name?></li>
-                <?php endforeach; ?>
-                <div class="madel-slide second_adaptive">
-                    <?php
-                    if ( $image_array ) {
+        $isCat = false;
+        if (in_array($category->term_id, $categoriesModel)) {
+            $isCat = true;
+        }
+        ?>
+        <?php $class = $isCat ? 'class="active-cat"': '' ?>
+        <li <?=$class?>><?=$category->name?></li>
+    <?php endforeach; ?>
+</ul>
+<div class="model-video video-mobile">
+        <video src="<?php the_field('video_mdls'); ?>" autoplay loop controls></video>
+    </div>
 
-                        foreach ( $image_array as $image ) {
+<div class="mdl__descriotion">
+    <p><?=$about?></p>
+</div>
+<ul class="mdl__spec_info">
+    <li class="li">
+        <div>Height:</div>
+        &nbsp;
+        <p><?=$height?>cm</p>
+    </li>
+    <li class="li">
+        <div>Bust:</div>
+        &nbsp;
+        <p><?=$bust?>cm</p>
+    </li>
+    <li class="li">
+        <div>Waist:</div>
+        &nbsp;
+        <p><?=$waist?>cm</p>
+    </li>
+    <li class="li">
+        <div>Hips:</div>
+        &nbsp;
+        <p><?=$hips?>cm</p>
+    </li>
+    <li class="li">
+        <div>Age:</div>
+        &nbsp;
+        <p><?=$age?>cm</p>
+    </li>
+</ul>
+<!-- <ul class="madel-info-list">
+    <li><dl><dt>Height:</dt><dd><?=$height?>cm</dd></dl></li>
+    <li><dl><dt>Bust:</dt><dd><?=$bust?>cm</dd></dl></li>
+    <li><dl><dt>Waist:</dt><dd><?=$waist?>cm</dd></dl></li>
+    <li><dl><dt>Hips:</dt><dd><?=$hips?>cm</dd></dl></li>
+    <li><dl><dt>Hair:</dt><dd><?=$hair?></dd></dl></li>
+    <li><dl><dt>Eyes:</dt><dd><?=$eyes?></dd></dl></li>
+    <li><dl><dt>Age:</dt><dd><?=$age?></dd></dl></li>
 
-                            $fullimg = pods_image_url( $image['ID'], 'slideBig');
-
-                            ?>
-                            <div><img src="<?=$fullimg?>" alt=""></div>
-                    <?php
-                        }
-                    }
-                    ?>
-                </div>
-            </ul>
-            <ul class="madel-info-list">
-                <li><dl><dt>Height:</dt><dd><?=$height?>cm</dd></dl></li>
-                <li><dl><dt>Bust:</dt><dd><?=$bust?>cm</dd></dl></li>
-                <li><dl><dt>Waist:</dt><dd><?=$waist?>cm</dd></dl></li>
-                <li><dl><dt>Hips:</dt><dd><?=$hips?>cm</dd></dl></li>
-                <li><dl><dt>Hair:</dt><dd><?=$hair?></dd></dl></li>
-                <li><dl><dt>Eyes:</dt><dd><?=$eyes?></dd></dl></li>
-                <li><dl><dt>Age:</dt><dd><?=$age?></dd></dl></li>
-
-            </ul>
-            <div class="model__buttons">
-                <a id="js-selected" data-id="<?=$id?>" href="#" class='model__btn add cart-button blue '>Add to list</a>
-                <a id="orderNow" data-id="<?=$id?>" href="#" class='model__btn order cart-button'>Order now</a>
-            </div>
-        </div>
-        
+</ul> -->
+<div class="model__buttons">
+    <!-- <a id="orderNow" data-id="<?=$id?>" href="#" class='model__btn order cart-button'>Order now</a> -->
+    <a id="orderNow" data-id="<?=$id?>" href="#" class='add_to_list_order'>
+        <span class="plus"><svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M28.8 0H1.2C0.53625 0 0 0.53625 0 1.2V28.8C0 29.4638 0.53625 30 1.2 30H28.8C29.4638 30 30 29.4638 30 28.8V1.2C30 0.53625 29.4638 0 28.8 0ZM22.2 15.9C22.2 16.065 22.065 16.2 21.9 16.2H16.2V21.9C16.2 22.065 16.065 22.2 15.9 22.2H14.1C13.935 22.2 13.8 22.065 13.8 21.9V16.2H8.1C7.935 16.2 7.8 16.065 7.8 15.9V14.1C7.8 13.935 7.935 13.8 8.1 13.8H13.8V8.1C13.8 7.935 13.935 7.8 14.1 7.8H15.9C16.065 7.8 16.2 7.935 16.2 8.1V13.8H21.9C22.065 13.8 22.2 13.935 22.2 14.1V15.9Z" fill="#FFB300"/>
+        </svg>
+        </span>
+        Order model
+    </a>
+</div>
+</div>
+    <div class="model-video">
+        <video src="<?php the_field('video_mdls'); ?>" autoplay loop controls></video>
+    </div>
     </section>
 
     <section class="models-similar">
@@ -222,10 +243,16 @@ $id = get_the_ID();
 
 
 </div>
+<script>
 
+jQuery(document).ready(function () {
+    jQuery('iframe').contents().find('#mdlsVideo').css({'display':'none'});
+});
+</script>
 <script src="<?=$path?>/js/slick.min.js"></script>
 <script src="<?=$path?>/js/jquery.notifyBar.js"></script>
 <?php
     get_footer();
 ?>
+
 
